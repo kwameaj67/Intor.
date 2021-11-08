ad368ad908cf29088056311884ad0183c3886243
Kwame <kwameagyenimboateng64@gmail.com>
2021-11-08 15:45:45 +0000

    added loader

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import './App.css';
-import Home from './pages/Home/Home'
+import React, { Suspense } from 'react'
+
+const Home = React.lazy(() => import('./pages/Home/Home'))
 
 function App() {
   return (
-    <div>
-      <Home/>
+    <div >
+      <Suspense fallback={
+        <div className="App">
+          <div className="loader">
+            <h1>intor.</h1>
+          </div>
+        </div>
+      }>
+        <Home />
+      </Suspense>
     </div>
   );
 }
